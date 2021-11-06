26c4de9b16c085e17e9c576a0b115c467bea0853
Andrzej Fidut <s24728@szuflandia.pjwstk.edu.pl>
2021-11-06 17:23:05 +0100

    zmiany u kolegi

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
+
 #include<iostream>
 
 int main()
 {
-  int a, b, c;
-  std::cout << "Podaj 3 liczby calkowite " << std::endl;
-  std::cin >> a >> b >> c;
-  std::cout << (a + b + c) / 3; << std::endl;
+  int a, b, c, d;
+  std::cout << "Podaj 4 liczby calkowite " << std::endl;
+  std::cin >> a >> b >> c, d;
+  std::cout << (a + b + c + d) / 4; << std::endl;
   return 0;
 }
